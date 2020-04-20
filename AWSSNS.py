c87bfdb54356700af7963967c19a98accb5bfe56
chaimstanton1@gmail.com <38401049+ChZeSt@users.noreply.github.com>
2020-04-20 23:13:36 +0100

    pep8

--- AWSSNS.py
+++ AWSSNS.py
@@ -5,4 +5,5 @@ from botocore.exceptions import ClientError
 
 sns = boto3.resource('sns')
 topic = sns.Topic('arn:aws:sns:eu-west-1:696210664380:tescoBot')
-topic.publish(Message="hey this is a test using the api", Subject="test messgae")
+topic.publish(Message="hey this is a test using the api",
+              Subject="test messgae")
