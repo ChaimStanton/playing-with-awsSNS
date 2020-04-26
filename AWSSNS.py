5ccbe50dd8ecc457b6b9f670f3ecd472fda54014
chaimstanton1@gmail.com <38401049+ChZeSt@users.noreply.github.com>
2020-04-26 03:15:13 +0100

    added datetime

--- AWSSNS.py
+++ AWSSNS.py
@@ -2,8 +2,9 @@
 
 import boto3
 from botocore.exceptions import ClientError
+import datetime
 
 sns = boto3.resource('sns')
 topic = sns.Topic('arn:aws:sns:eu-west-1:696210664380:tescoBot')
-topic.publish(Message="hey this is a test using the api",
+topic.publish(Message="hey this is a test using the api sent at " + str(datetime.datetime.now()),
               Subject="test messgae")
